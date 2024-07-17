c38e55f629cae470ab914eb7cd57cdd02e74ccfd
KiranBai <kiranbai10091999@gmail.com>
2024-07-17 18:51:14 +0500

    Adding Controller helper models folder for User login maintainity

--- Backend_Server/index.js
+++ Backend_Server/index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/connectDB')
+const router = require('./routes/index')
+const cookiesParser = require('cookie-parser')
 const app = express()
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
 }))
+
+app.use(express.json())
+app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
 
@@ -15,6 +20,10 @@ app.get('/',(request,response)=>{
         message : "Server running at " + PORT
     })
 })
+
+
+//api endpoints
+app.use('/api',router)
 
 connectDB().then(()=>{
 
